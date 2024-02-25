faebb4dbab2ca57903f4dfbbacb5c53a0c55e5b6
maestroi <meastro@protonmail.com>
2024-02-25 02:13:53 +0100

    Initial implementation

---
+++ api/main.py
@@ -0,0 +1,107 @@
+from typing import List, Optional
+from fastapi import FastAPI, HTTPException, APIRouter
+from fastapi.openapi.docs import get_swagger_ui_html
+from pydantic import BaseModel, Field
+from pymongo import MongoClient
+from datetime import datetime
+
+app = FastAPI(docs_url=None, redoc_url=None)  # Disable default docs
+
+client = MongoClient("mongodb://mongo:27017/")  # Adjust if necessary
+db = client.blockchainDB
+router = APIRouter()
+
+class Transaction(BaseModel):
+    hash: str = Field(..., example="293cb3f47fc9da3cdd6b3cbf596dd81409d708374384bc8e1f9c7dc08303f855")
+    blockNumber: int = Field(..., example=17043344)
+    timestamp: int = Field(..., example=1708815974854)
+    confirmations: int = Field(..., example=47)
+    from_: str = Field(..., alias="from", example="NQ81 C01N BASE 0000 0000 0000 0000 0000 0000")
+    fromType: int = Field(..., example=0)
+    to: str = Field(..., example="NQ57 M1NT JRQA FGD2 HX1P FN2G 611P JNAE K7HN")
+    toType: int = Field(..., example=0)
+    value: int = Field(..., example=5554230)
+    fee: int = Field(..., example=0)
+    senderData: str = Field(..., example="")
+    recipientData: str = Field(..., example="")
+    flags: int = Field(..., example=0)
+    validityStartHeight: int = Field(..., example=17043344)
+    proof: str = Field(..., example="")
+    networkId: int = Field(..., example=5)
+    executionResult: bool = Field(..., example=True)
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "hash": "293cb3f47fc9da3cdd6b3cbf596dd81409d708374384bc8e1f9c7dc08303f855",
+                "blockNumber": 17043344,
+                "timestamp": 1708815974854,
+                "confirmations": 47,
+                "from": "NQ81 C01N BASE 0000 0000 0000 0000 0000 0000",
+                "fromType": 0,
+                "to": "NQ57 M1NT JRQA FGD2 HX1P FN2G 611P JNAE K7HN",
+                "toType": 0,
+                "value": 5554230,
+                "fee": 0,
+                "senderData": "",
+                "recipientData": "",
+                "flags": 0,
+                "validityStartHeight": 17043344,
+                "proof": "",
+                "networkId": 5,
+                "executionResult": True
+            }
+        }
+
+class CountResponse(BaseModel):
+    count: int
+
+@app.get("/transactions/{block_number}", response_model=List[Transaction])
+def read_transactions(block_number: int):
+    transactions = list(db.transactions.find({"blockNumber": block_number}, {'_id': False}))
+    if transactions:
+        return transactions
+    raise HTTPException(status_code=404, detail="Transactions not found")
+
+@app.get("/transactions/hash/{tx_hash}", response_model=Transaction)
+def read_transaction_by_hash(tx_hash: str):
+    transaction = db.transactions.find_one({"hash": tx_hash}, {'_id': False})
+    if transaction:
+        return transaction
+    raise HTTPException(status_code=404, detail="Transaction not found")
+
+@app.get("/transactions/address/{address}", response_model=List[Transaction])
+def read_transactions_by_address(address: str, limit: int = 50):
+    transactions = list(db.transactions.find({"$or": [{"from": address}, {"to": address}]}, {'_id': False}).limit(limit))
+    if transactions:
+        return transactions
+    raise HTTPException(status_code=404, detail="Transactions not found")
+
+@app.get("/transactions/count/all", response_model=CountResponse)
+def get_transactions_count():
+    count = db.transactions.count_documents({})
+    return CountResponse(count=count)
+
+@app.get("/transactions/date", response_model=List[Transaction])
+def read_transactions_by_date(start_date: str, end_date: str, limit: int = 50):
+    start_timestamp = datetime.strptime(start_date, "%Y-%m-%d").timestamp()
+    end_timestamp = datetime.strptime(end_date, "%Y-%m-%d").timestamp()
+    transactions = list(db.transactions.find({
+        "timestamp": {"$gte": start_timestamp, "$lte": end_timestamp}
+    }, {'_id': False}).limit(limit))
+    if transactions:
+        return transactions
+    raise HTTPException(status_code=404, detail="Transactions not found")
+
+app.include_router(router, prefix="/api/v1")
+
+@app.get("/docs", include_in_schema=False)
+def custom_swagger_ui_html():
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="Custom Swagger UI")
+
+# Add more endpoints as necessary for different query capabilities
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ indexer/indexer.py
@@ -0,0 +1,152 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import requests
+import time
+from pymongo import MongoClient
+import os
+import logging
+
+# Logging configuration
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s — %(message)s',
+                    datefmt='%Y-%m-%d_%H:%M:%S',
+                    handlers=[logging.StreamHandler()])
+
+# Configuration
+BLOCK_TIME = 1  # Block time in seconds
+START_BLOCK = 16335524
+MONGO_URI = "mongodb://mongo:27017/"  # Adjust if you have a different MongoDB URI
+DB_NAME = "blockchainDB"
+COLLECTION_NAME = "transactions"
+RPC_URL = os.getenv("RPC_URL", "https://rpc-testnet.nimiqcloud.com/")  # Configurable RPC URL
+
+
+def update_last_indexed_block(block_number, db):
+    db.lastIndexedBlock.update_one(
+        {"id": "lastIndexedBlock"},
+        {"$set": {"block_number": block_number}},
+        upsert=True
+    )
+
+def get_last_indexed_block(db):
+    last_block_doc = db.lastIndexedBlock.find_one({"id": "lastIndexedBlock"})
+    if last_block_doc:
+        return last_block_doc['block_number']
+    else:
+        return None
+
+def fetch_latest_block_number():
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getBlockNumber",
+        "params": []
+    }
+    response = requests.post(RPC_URL, json=payload)
+    if response.status_code == 200:
+        data = response.json()
+        return data['result']['data']
+    else:
+        return None
+
+def fetch_genesis_block_number():
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "getPolicyConstants",
+        "params": [],
+        "id": 0
+    }
+    response = requests.post(RPC_URL, json=payload)
+    if response.status_code == 200:
+        data = response.json()
+        return data['result']['data']['genesisBlockNumber']
+    else:
+        return None
+
+def fetch_transactions_by_block(block_number):
+    payload = {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "getTransactionsByBlockNumber",
+        "params": [block_number]
+    }
+    try:
+        response = requests.post(RPC_URL, json=payload, timeout=10)  # Added timeout for the request
+        response.raise_for_status()  # This will raise an exception for HTTP error responses
+        data = response.json()
+        if 'result' in data and 'data' in data['result']:
+            return data['result']['data']
+        else:
+            logging.warning(f"No transaction data found for block {block_number}.")
+            return None
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Request failed for block {block_number}: {e}")
+        return None
+
+def fetch_blocks_parallel(start_block, end_block, collection, latest_block, db, max_workers=25):
+    blocks_range = range(start_block, end_block + 1)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_block = {executor.submit(fetch_transactions_by_block, block_number): block_number for block_number in blocks_range}
+        for future in as_completed(future_to_block):
+            block_number = future_to_block[future]
+            try:
+                transactions = future.result()
+                if transactions:
+                    collection.insert_many(transactions, ordered=False)
+                    logging.info(f"{block_number} - Inserted {len(transactions)} transactions. Progress: {block_number}/{latest_block} ({latest_block - block_number} blocks left)")
+                    update_last_indexed_block(block_number, db)
+                else:
+                    logging.debug(f"{block_number} - No transactions found for block")
+            except pymongo.errors.BulkWriteError as e:
+                logging.error(f"Error inserting transactions for block {block_number}: {e.details}")
+                # Process successfully inserted transactions before the error occurred
+                update_last_indexed_block(block_number, db)
+
+def fetch_blocks_real_time(start_block, latest_block, collection, db):
+    current_block = start_block
+    while True:
+        transactions = fetch_transactions_by_block(current_block)
+        if transactions:
+            collection.transactions.insert_many(transactions)
+            logging.info(f"{current_block} - Inserted {len(transactions)} transactions. Progress: {current_block}/{latest_block} ({latest_block - current_block} blocks left)")
+        else:
+            logging.debug(f"{current_block} - No transactions found for block ")
+        current_block += 1
+        time.sleep(BLOCK_TIME)  # Respect the 1-second block time
+
+def main():
+    client = MongoClient(MONGO_URI)
+    db = client[DB_NAME]
+    collection = db[COLLECTION_NAME]
+
+    # Ensure the transactions collection has a unique index on the hash
+    collection.create_index([("hash", 1)], unique=True)
+    
+    genesis_block = fetch_genesis_block_number()
+    latest_block = fetch_latest_block_number()
+    
+    logging.info("Nimiq v2 TX Indexer")
+    logging.info(f"Genesis block: {genesis_block}")
+    logging.info(f"Latest block: {latest_block}")
+    
+    if genesis_block is None or latest_block is None:
+        logging.error("Failed to fetch blockchain constants.")
+        return
+    
+    last_indexed_block = get_last_indexed_block(db)
+    current_block = last_indexed_block if last_indexed_block else (START_BLOCK if START_BLOCK > genesis_block else genesis_block)
+
+    logging.info(f"Resuming from block: {current_block}")
+
+    # Parallel fetching if we're significantly behind
+    if latest_block - current_block > 25:  # Example threshold
+        logging.info("Starting parallel fetching...")
+        fetch_blocks_parallel(current_block, latest_block - 100, collection, latest_block, db, max_workers=20)
+        current_block = latest_block - 99
+
+    # Transition to real-time fetching
+    logging.info("Switching to real-time fetching...")
+    fetch_blocks_real_time(current_block, latest_block, collection, db)
+
+
+if __name__ == "__main__":
+    main()
